5df1f60a3125071999df977150ed377e3cdf6627
StyreXxX <rayhan37@iut-dhaka.edu>
2025-02-18 10:54:38 +0600

    Add files via upload

---
+++ flutter/Firebase/Chat App/lib/pages/register_page.dart
@@ -0,0 +1,110 @@
+import 'package:chat_app/services/auth/auth_service.dart';
+import 'package:chat_app/components/my_button.dart';
+import 'package:chat_app/components/my_textfield.dart';
+import 'package:flutter/material.dart';
+
+class RegisterPage extends StatelessWidget {
+  RegisterPage({super.key, required this.onTap});
+
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _confirmpasswordController =
+      TextEditingController();
+
+  final void Function()? onTap;
+  void register(BuildContext context) {
+    final _auth = AuthService();
+    if (_passwordController.text == _confirmpasswordController.text) {
+      try{
+        _auth.signUpWithEmailPassword(
+            _emailController.text, _passwordController.text);
+      } catch(e){
+        showDialog(
+            context: context,
+            builder: (context) => AlertDialog(
+                  title: Text(e.toString()),
+                ));
+        }
+    } 
+    else {
+      showDialog(
+            context: context,
+            builder: (context) => AlertDialog(
+                  title: Text("Passwords don't match"),
+                ));
+        }
+    }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.surface,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(Icons.message,
+                size: 60, color: Theme.of(context).colorScheme.primary),
+            SizedBox(
+              height: 50,
+            ),
+            Text(
+              "Lets create an account for you",
+              style: TextStyle(
+                  color: Theme.of(context).colorScheme.primary, fontSize: 16),
+            ),
+            SizedBox(
+              height: 25,
+            ),
+            MyTextField(
+              hintText: 'Email',
+              obscureText: false,
+              controller: _emailController,
+            ),
+            SizedBox(height: 10),
+            MyTextField(
+              hintText: 'Password',
+              obscureText: true,
+              controller: _passwordController,
+            ),
+            SizedBox(height: 10),
+            MyTextField(
+              hintText: 'Confirm Password',
+              obscureText: true,
+              controller: _confirmpasswordController,
+            ),
+            SizedBox(
+              height: 25,
+            ),
+            MyButton(
+              text: 'Register',
+              onTap:()=> register(context),
+            ),
+            SizedBox(
+              height: 25,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "Already a member?",
+                  style:
+                      TextStyle(color: Theme.of(context).colorScheme.primary),
+                ),
+                GestureDetector(
+                  onTap: onTap,
+                  child: Text(
+                    " Login",
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Theme.of(context).colorScheme.primary),
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
